4fecc0a857d1d6e2b9060ae904d2097b5bcd06b2
Michael Anyi <mikeyeucresta@gmail.com>
2019-12-22 21:46:58 +0100

    visualiztion changes

---
+++ fetch/script.js
@@ -0,0 +1,32 @@
+const fetch = require("node-fetch");
+setInterval(() => {
+  const randomTemp = Math.random() * 5 + 10;
+  fetch(`http://localhost:8080/v1alpha1/graphql`, {
+    method: "POST",
+    headers: {
+      "x-hasura-admin-secret": "mylongsecretkey"
+    },
+    body: JSON.stringify({
+      query: `
+            mutation ($temp: numeric) {
+              insert_temperature (
+                objects: [{
+                  temperature: $temp
+                  location: "London"
+                }]
+              ) {
+                returning {
+                  recorded_at
+                  temperature
+                }
+              }
+            }
+          `,
+      variables: {
+        temp: randomTemp
+      }
+    })
+  }).then(resp =>
+    resp.json().then(respObj => console.log(JSON.stringify(respObj, null, 2)))
+  );
+}, 2000);
